fe7fce305efbec29e18094031a408c6fb47b5787
Victoperealira <victorperealira99@gmail.com>
2025-08-25 19:38:41 +0200

    Modificacion conexion base de dato y mejora de seguridad

--- app/database.py
+++ app/database.py
@@ -3,8 +3,23 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://appbiblio:Eq_NdS2025@192.168.1.130:5432/biblioteca"
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()  
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Comprobacion de que las varaibles de enentorno se han cargado correctamente
+if not all( [DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME] ):
+    raise ValueError("Faltan variables de entorno necesarias para la conexión a la base de datos.")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{quote_plus(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -18,10 +33,4 @@ def get_db():
         db.close()
 
 
-"""try:
-    # Intentar conectar y ejecutar una consulta simple
-    with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1"))
-        print("Conexión OK:", result.scalar())
-except Exception as e:
-    print("Error en la conexión:", e)"""
+
