962d3580d012fd7cf9bfbb7149f2535cfadedf8b
Victoperealira <victorperealira99@gmail.com>
2025-08-26 08:13:47 +0200

    probando commit sin FastAPI

--- app/database.py
+++ app/database.py
@@ -9,15 +9,17 @@ import os
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()  
 
+# Obtener las variables de entorno necesarias para la conexión a la base de datos del fichero .env PostgreSQL
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# Comprobacion de que las varaibles de enentorno se han cargado correctamente
+# Comprobacion de que las varaibles de enentorno se han cargado correctamente.
 if not all( [DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME] ):
     raise ValueError("Faltan variables de entorno necesarias para la conexión a la base de datos.")
+
 
 DATABASE_URL = f"postgresql://{DB_USER}:{quote_plus(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
